040d77636e6e50d058efe61101ee05600340d2a7
HattaMaulana <mahatta.maulana@gmail.com>
2019-05-25 20:04:52 +0700

    secure_asset

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,26 +7,26 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="{{ asset('theme/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('theme/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href=" {{ asset('theme/bower_components/font-awesome/css/font-awesome.min.css') }} ">
+  <link rel="stylesheet" href=" {{ secure_asset('theme/bower_components/font-awesome/css/font-awesome.min.css') }} ">
   <!-- Ionicons -->
-  <link rel="stylesheet" href=" {{ asset('theme/bower_components/Ionicons/css/ionicons.min.css') }} ">
+  <link rel="stylesheet" href=" {{ secure_asset('theme/bower_components/Ionicons/css/ionicons.min.css') }} ">
   <!-- Theme style -->
-<link rel="stylesheet" href=" {{ asset('theme/css/AdminLTE.min.css') }} ">
+<link rel="stylesheet" href=" {{ secure_asset('theme/css/AdminLTE.min.css') }} ">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href=" {{ asset('theme/css/skins/_all-skins.min.css') }} ">
+  <link rel="stylesheet" href=" {{ secure_asset('theme/css/skins/_all-skins.min.css') }} ">
   <!-- Morris chart -->
-  <link rel="stylesheet" href=" {{ asset('theme/bower_components/morris.js/morris.css') }} ">
+  <link rel="stylesheet" href=" {{ secure_asset('theme/bower_components/morris.js/morris.css') }} ">
   <!-- jvectormap -->
-  <link rel="stylesheet" href=" {{ asset('theme/bower_components/jvectormap/jquery-jvectormap.css') }} ">
+  <link rel="stylesheet" href=" {{ secure_asset('theme/bower_components/jvectormap/jquery-jvectormap.css') }} ">
   <!-- Date Picker -->
-  <link rel="stylesheet" href=" {{ asset('theme/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }} ">
+  <link rel="stylesheet" href=" {{ secure_asset('theme/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }} ">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href=" {{ asset('theme/bower_components/bootstrap-daterangepicker/daterangepicker.css') }} ">
+  <link rel="stylesheet" href=" {{ secure_asset('theme/bower_components/bootstrap-daterangepicker/daterangepicker.css') }} ">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href=" {{ asset('theme/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }} ">
+  <link rel="stylesheet" href=" {{ secure_asset('theme/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }} ">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -102,13 +102,13 @@
           
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="{{ asset('theme/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
+              <img src="{{ secure_asset('theme/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
               <span class="hidden-xs">Alexander Pierce</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="{{ asset('theme/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+                <img src="{{ secure_asset('theme/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
 
                 <p>
                   Alexander Pierce - Web Developer
